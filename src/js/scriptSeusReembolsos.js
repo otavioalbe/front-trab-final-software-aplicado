71055e7137999eb99c625107773b11b7ee321698
Otávio Albuquerque <otavioalbe@gmail.com>
2023-11-28 16:29:54 -0300

    visualizar SEUS reembolsos pronto

---
+++ src/js/scriptSeusReembolsos.js
@@ -0,0 +1,26 @@
+function carregarSeusReembolsos(){
+    fetch('http://localhost:8080/reembolso/buscar?size=100') //+ $('#page').val())
+    .then(response => response.json())
+    .then(reembolsos => {
+        //console.log(reembolsos)
+        data = reembolsos.content
+        var lista = document.querySelector('#repos')
+        for(i in data){
+            //console.log(data[i])
+            var objeto = document.createElement('li')
+            if(reembolsos.content[i].funcionarioId == localStorage.getItem('idFuncionario')){
+            objeto.innerHTML = JSON.stringify(
+                "<strong>Reembolso "+reembolsos.content[i].id +"</strong>")+ 
+                "<br>Descrição: " + reembolsos.content[i].descricao + 
+                "<br> - Valor: " + reembolsos.content[i].valor +
+                "<br> - ID do funcionário: " + reembolsos.content[i].funcionarioId +
+                "<br> - Data de solicitação: " + reembolsos.content[i].dataSolicitacao +
+                "<br> - Data de resposta: " + reembolsos.content[i].dataResposta +
+                "<br> - Status: " + reembolsos.content[i].status +
+                "<br> - Motivo: "+ reembolsos.content[i].motivoRecusa + "<br><br>"
+            lista.appendChild(objeto)
+            }
+    }
+}).catch(error => console.log(error))
+}
+carregarSeusReembolsos()
